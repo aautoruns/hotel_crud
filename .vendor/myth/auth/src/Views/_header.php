3033383beb4e8d41c9128d87634b91523823534c
aautoruns <galihazella06@gmail.com>
2023-01-02 21:04:40 +0700

    bug edit tamu

---
+++ .vendor/myth/auth/src/Views/_header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+    <title>Myth:Auth</title>
+    <style>
+        body {
+            padding-top: 10rem;
+        }
+    </style>
+</head>
+
+<body>
+    <?= view('Myth\Auth\Views\_navbar') ?>
+
+    <main role="main" class="container">
